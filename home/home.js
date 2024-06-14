3e80972ef3d95572892b6dd293c343b881498113
Goel Nkoko <goelnkoko12@hotmail.com>
2024-06-14 01:44:16 +0100

    Separado as responsabilidades das páginas de menu (leftBar), outros (rightBar) e Feed. A inclusão do menu e outros agora é feito pelo javascript

--- home/home.js
+++ home/home.js
@@ -1,6 +1,6 @@
 function adjustTextarea(el) {
     el.style.height = 'auto';
-    el.style.height = (el.scrollHeight) + 'px';
+    el.style.height = (el.scrollHeight - '40') + 'px';
 }
 
 window.addEventListener('scroll', function() {
@@ -15,123 +15,128 @@ window.addEventListener('scroll', function() {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    const leftBar = document.querySelector('.leftBar');
-    const rightBar = document.querySelector('.rightBar');
-    const feed = document.querySelector('.feed');
-    const logo = document.querySelector('#logo');
-    const menu_home = document.querySelector('#menu');
-    const menu_profile = document.querySelector('.user-profile');
-    const dropZone = document.getElementById('drop-zone');
-    const fileInput = document.getElementById('file-input');
-    const uploadButton = document.getElementById('upload-button');
-    const thumbnails = document.getElementById('thumbnails');
-    const sendButton = document.getElementById('send-button');
-    const postContent = document.getElementById('post');
-    const postsContainer = document.getElementById('posts-container');
+    const observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.type === 'childList') {
+                const body = document.querySelector('body');
+                const leftBar = document.querySelector('.leftBar');
+                const rightBar = document.querySelector('.rightBar');
+                const feed = document.querySelector('.feed');
+                const logo = document.querySelector('#logo');
+                const menu_home = document.querySelector('#menu');
+                const menu_profile = document.querySelector('.user-profile');
+                const dropZone = document.getElementById('drop-zone');
+                const fileInput = document.getElementById('file-input');
+                const uploadButton = document.getElementById('upload-button');
+                const thumbnails = document.getElementById('thumbnails');
+                const sendButton = document.getElementById('send-button');
+                const postContent = document.getElementById('post');
+                const postsContainer = document.getElementById('posts-container');
 
-    leftBar.style.height = '100vh';
-    rightBar.style.height = '100vh';
+                leftBar.style.height = '100vh';
+                rightBar.style.height = '100vh';
 
-    const width = leftBar.offsetWidth;
-    feed.style.marginLeft = `${width}px`;
-    feed.style.marginRight = `${width}px`;
-    rightBar.style.left = width + feed.offsetWidth + 'px';
+                const widthLeft = leftBar.offsetWidth;
+                const widthRight = rightBar.offsetWidth;
 
-    const menu_home_size = menu_home.offsetWidth;
-    logo.style.width = menu_home_size + 'px';
-    menu_profile.style.width = (menu_home_size * 1.1) + 'px';
+                body.style.paddingLeft = `${widthLeft}px`;
+                body.style.paddingRight = `${widthRight}px`;
+            
+                const menu_home_size = menu_home.offsetWidth;
+                logo.style.width = menu_home_size +30+ 'px';
+                menu_profile.style.width = (menu_home_size * 1.1) + 'px';
 
-    logo.addEventListener('click', () => {
-        window.location.href = "../html/home.html";
-    });
+                logo.addEventListener('click', () => {
+                    window.location.href = "../home/home.html";
+                });
 
-    menu_home.addEventListener('click', () => {
-        window.location.href = "../html/home.html";
-    });
+                menu_home.addEventListener('click', () => {
+                    window.location.href = "../home/home.html";
+                });
 
-    menu_profile.addEventListener('click', () => {
-        window.location.href = "../html/profile.html";
-    });
+                menu_profile.addEventListener('click', () => {
+                    window.location.href = "../perfil/perfil.html";
+                });
 
-    uploadButton.addEventListener('click', () => {
-        fileInput.click();
-    });
+                uploadButton.addEventListener('click', () => {
+                    fileInput.click();
+                });
 
-    fileInput.addEventListener('change', (event) => {
-        const files = event.target.files;
-        handleFileUploads(files);
-    });
+                fileInput.addEventListener('change', (event) => {
+                    const files = event.target.files;
+                    handleFileUploads(files);
+                });
 
-    sendButton.addEventListener('click', () => {
-        // Aqui você pode enviar a postagem para o backend
-        const postText = postContent.value;
-        const images = Array.from(thumbnails.querySelectorAll('img')).map(img => img.src);
-        createPost(postText, images);
-    });
+                sendButton.addEventListener('click', () => {
+                    const postText = postContent.value;
+                    const images = Array.from(thumbnails.querySelectorAll('img')).map(img => img.src);
+                    createPost(postText, images);
+                });
 
-    function handleFileUploads(files) {
-        for (let file of files) {
-            const reader = new FileReader();
-            reader.onload = function(event) {
-                const thumbnail = createThumbnail(event.target.result);
-                thumbnails.appendChild(thumbnail);
-            }
-            reader.readAsDataURL(file);
-        }
-    }
+                function handleFileUploads(files) {
+                    for (let file of files) {
+                        const reader = new FileReader();
+                        reader.onload = function(event) {
+                            const thumbnail = createThumbnail(event.target.result);
+                            thumbnails.appendChild(thumbnail);
+                        }
+                        reader.readAsDataURL(file);
+                    }
+                }
 
-    function createThumbnail(src) {
-        const thumbnail = document.createElement('div');
-        thumbnail.classList.add('thumbnail');
+                function createThumbnail(src) {
+                    const thumbnail = document.createElement('div');
+                    thumbnail.classList.add('thumbnail');
 
-        const img = document.createElement('img');
-        img.src = src;
+                    const img = document.createElement('img');
+                    img.src = src;
 
-        const removeButton = document.createElement('button');
-        removeButton.classList.add('remove');
-        removeButton.textContent = '×';
-        removeButton.addEventListener('click', () => {
-            thumbnail.remove();
-        });
+                    const removeButton = document.createElement('button');
+                    removeButton.classList.add('remove');
+                    removeButton.textContent = '×';
+                    removeButton.addEventListener('click', () => {
+                        thumbnail.remove();
+                    });
 
-        thumbnail.appendChild(img);
-        thumbnail.appendChild(removeButton);
+                    thumbnail.appendChild(img);
+                    thumbnail.appendChild(removeButton);
 
-        return thumbnail;
-    }
+                    return thumbnail;
+                }
 
-    function createPost(text, images) {
-        const postTemplate = `
-            <div class="post">
-                <div class="post-user-profile">
-                    <a href="../perfil/perfil.html">
-                        <img src="../img/gyomei-chorando.jpeg" alt="Foto do Gyomei">
-                        <div id="profile-content">
-                            <span>Nkumbo</span>
-                            <p>SOGA Hater</p>
+                function createPost(text, images) {
+                    const postTemplate = `
+                        <div class="post">
+                            <div class="post-user-profile">
+                                <a href="../perfil/perfil.html">
+                                    <img src="../img/gyomei-chorando.jpeg" alt="Foto do Gyomei">
+                                    <div id="profile-content">
+                                        <span>Pedro</span>
+                                        <p>Administrador</p>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="post-content">
+                                <p>${text}</p>
+                                ${images.map(image => `<img src="${image}" alt="Post Image">`).join('')}
+                            </div>
+                            <div class="icons">
+                                <button><span class="material-symbols-outlined material-style">thumb_up</span></button>
+                                <button><span class="material-symbols-outlined material-style">comment</span></button>
+                                <button><span class="material-symbols-outlined material-style">share</span></button>
+                            </div>
                         </div>
-                    </a>
-                </div>
-                <div class="post-content">
-                    <p>${text}</p>
-                    ${images.map(image => `<img src="${image}" alt="Post Image">`).join('')}
-                </div>
-                <div class="icons">
-                    <button><span class="material-symbols-outlined material-style">thumb_up</span></button>
-                    <button><span class="material-symbols-outlined material-style">comment</span></button>
-                    <button><span class="material-symbols-outlined material-style">share</span></button>
-                </div>
-            </div>
-        `;
+                    `;
 
-        postsContainer.insertAdjacentHTML('beforeend', postTemplate);
+                    postsContainer.insertAdjacentHTML('beforeend', postTemplate);
 
-        postContent.value = '';
-        thumbnails.innerHTML = '';
-    }
+                    postContent.value = '';
+                    thumbnails.innerHTML = '';
+                }
+            }
+            
+        });
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
 });
-
-function adjustTextarea(textarea) {
-    textarea.style.height = 'auto';
-    textarea.style.height = (textarea.scrollHeight) + 'px';
-}
